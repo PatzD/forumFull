69696a8ca46c861110f3a4ca7365c7bc7ea9b2c5
patrykD <zakos123456@gmail.com>
2022-04-14 01:06:00 +0200

    added registration and confirmation msg, need login + error msgs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from requests import request
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+
 from .models import Post
+from .forms import CreateUserForm
 
 def index(request):
     posts = Post.objects.all()
@@ -13,4 +18,17 @@ def login_page(request):
     return render(request, 'login.html')
 
 def signup_page(request):
-    return render(request, 'signup.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = form.cleaned_data.get('username')
+
+            messages.success(request, 'Account successfully created for '+user)
+            return redirect("login.html")
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
